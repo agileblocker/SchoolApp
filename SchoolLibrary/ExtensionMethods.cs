0c0a22b5daf74bcf8dc8c4cbbe6fe4fbeba2ab47
Ryan Miller <ryan.miller@nowpow.com>
2018-11-17 12:31:00 -0600

    First
    First commit to upload "finished" project from this course.

---
+++ SchoolLibrary/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static int WordCount(this string str)
+        {
+            var wordCount = str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            return wordCount;
+        }
+    }
+}
